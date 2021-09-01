78dcd3cb8a5e3c182ab3ba4ecb4a11a627e51170
orelm32 <orelm32@gmail.com>
2021-09-01 09:29:06 +0300

    check

--- udemy_memory_datasource.py
+++ udemy_memory_datasource.py
@@ -5,7 +5,7 @@ class MemoryDataSource:
     def __init__(self):
         self.reminders = dict()
 
-    def add_reminder(self,chat_id,message,time):
-        message_data = ReminderData(message,time)
-        self.reminders[chat_id]= message_data
+    def add_reminder(self,chat_id, message, time):
+        message_data = ReminderData(message, time)
+        self.reminders[chat_id] = message_data
         return message_data
